3154a609505e1b0bf4ffdd0f2f27415811a88e62
ArthurGas <21a.gastiev07@gmail.com>
2023-12-13 18:43:53 +0300

    Программа для хэширования

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -3,6 +3,7 @@
 #include <cryptopp/files.h>
 #define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
 #include <cryptopp/md5.h>
+#include <random>
 #include <system_error>
 
 namespace cpp = CryptoPP;
@@ -14,9 +15,11 @@ std::string hash(std::string file_name)
         if(file.peek() == std::ifstream::traits_type::eof())
             throw std::length_error("File is empty");
         while(getline(file, buf)) {
-            cpp::StringSource((buf,
-                              true,
-                              new cpp::HexEncoder(new cpp::StringSink(h_str)));
+            cpp::Weak1::MD5 hash;
+            cpp::StringSource ss( buf, true /* PumpAll */,
+                                  new cpp::HashFilter( hash,
+                                          new cpp::HexEncoder(new cpp::StringSink(h_str)))
+                                );
             res+=h_str;
         }
         file.close();
@@ -25,11 +28,11 @@ std::string hash(std::string file_name)
         throw std::system_error(errno, std::generic_category(), "Base read error");
     }
 }
-int main(){
-    try{
-        hash("file.txt");
-    }
-    catch(const std::exception &e){
+int main()
+{
+    try {
+        std::cout<<hash("file.txt")<<'\n';
+    } catch(const std::exception &e) {
         std::cerr<<e.what()<<std::endl;
     }
     return 0;
